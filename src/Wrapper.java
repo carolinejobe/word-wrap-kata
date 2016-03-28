c2067e42725cbe431288a187816d632fc287ba26
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-28 19:05:10 -0400

    adding more comments; cleaning up code

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -2,21 +2,25 @@
 public class Wrapper {
 
 	static String wrap(String input, int columnNumber) {
+		//converting String to char array to use for determining index of whitespace
 		char[] inputAsArray = input.toCharArray();
+		//initializing empty string to use for output
 		String output = "";
-		int lineBreakIndex=0;
 
+		//determine whether line breaks are needed with if statement
 		if (input.length() > columnNumber) {
+			//check for whitespace traversing string backwards from an index that is based on # of columns and index of
+			//last line break
 			for (int i = columnNumber; i<=(input.length()); i+=columnNumber) {
-				lineBreakIndex = input.lastIndexOf(" ", i); //determine the index of the last carriage prior to exceeding column count
-				inputAsArray[lineBreakIndex] = '\n'; //replace the whitespace with carriage return
-				i=lineBreakIndex; //reset i to where the carriage return was inserted
+				int lineBreakIndex = input.lastIndexOf(" ", i); //determine the index of the last whitespace preceding end of line
+				inputAsArray[lineBreakIndex] = '\n'; //replace the whitespace with line break
+				i=lineBreakIndex; //reset i to where the line break was inserted
 			}
 			output = String.valueOf(inputAsArray);
 			System.out.println(output);
 			return output;
 			
-		//if String is shorter than number of columns, no need to insert carriage returns	
+		//if String is shorter than number of columns, no need to insert line breaks
 		} else {
 			output = input;
 			System.out.println(output);
