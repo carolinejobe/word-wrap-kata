e1a30bb1ca35bcdf59c5f09c1bb64fe5005130d7
carolinejobe <caroline.h.jobe@gmail.com>
2016-03-28 17:06:27 -0400

    adding logic to insert first line break; need to add for subsequent line breaks

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -1,4 +1,24 @@
 
 public class Wrapper {
 
+	static String wrap(String input, int columnNumber){
+		char[] inputAsArray = input.toCharArray();
+		String output = "";
+		
+		//TODO wrap in for loop that will iterate over entire input and use counter of value 'columnnNumber' to find
+		//all line breaks
+		if(input.length()>columnNumber){
+			int lineBreakIndex = input.lastIndexOf(" ", columnNumber-1);
+			inputAsArray[lineBreakIndex]= '\n';
+			output = String.valueOf(inputAsArray);
+			System.out.println(output);
+			return output;
+		}
+		else{
+			output = input;
+			System.out.println(output);
+			return output;
+		}
+		
+	}
 }
--- src/wrapperApp.java
+++ src/wrapperApp.java
@@ -1,9 +1,15 @@
+/*You write a class called Wrapper, that has a single static function named wrap 
+ * that takes two arguments, a string, and a column number. The function returns the string, 
+ * but with line breaks inserted at just the right places to make sure that no line is 
+ * longer than the column number. You try to break lines at word boundaries.
+ * Like a word processor, break the line by replacing the last space in a line with a newline. */
 
 public class wrapperApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "This is my test, we'll see how it goes";
+		int columnNumber = 25;
+		Wrapper.wrap(input, columnNumber);
 	}
 
 }
